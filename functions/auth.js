61c4f19d6875a583fa82caef101650e0ad2acfb2
Delta234vcode <uberserhii16@gmail.com>
2025-06-03 10:41:54 +0100

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -6,16 +6,15 @@ const router = express.Router();
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
+passport.use(new DiscordStrategy({
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: process.env.DISCORD_CALLBACK_URL,
+    scope: ['identify']
+}, async (accessToken, refreshToken, profile, done) => {
+    // ...
+}));
 
-passport.use(
-  new DiscordStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-clientSecret: process.env.CLIENT_SECRET,
-callbackURL: process.env.REDIRECT_URI,
-      scope: ["identify"], // Залишаємо один scope
-    },
-    (accessToken, refreshToken, profile, done) => { // Залишаємо один verify callback
       process.nextTick(() => done(null, profile));
     }
   )
